c73e6a9736a70048baf1b68533b39f52a624dcbf
ChernetsovNG <n.chernetsov86@gmail.com>
2021-10-21 00:00:30 +0300

    Правильно заработало разложение по цветовым каналам

--- image_processing/src/ru/nchernetsov/Main.java
+++ image_processing/src/ru/nchernetsov/Main.java
@@ -3,6 +3,7 @@ package ru.nchernetsov;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
@@ -28,9 +29,9 @@ public class Main {
         // red channel
         int[] arrayResultOnlyRed = new int[image.getWidth() * image.getHeight()];
         int indexRed = 0;
-        for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
-            for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {
-                int rgb = getRGB(0, array2DRed[xPixel][yPixel], 0, 0);
+        for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {
+            for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
+                int rgb = getRGB(array2DAlpha[xPixel][yPixel], array2DRed[xPixel][yPixel], 0, 0);
                 arrayResultOnlyRed[indexRed] = rgb;
                 indexRed += 1;
             }
@@ -42,9 +43,9 @@ public class Main {
         // green channel
         int[] arrayResultOnlyGreen = new int[image.getWidth() * image.getHeight()];
         int indexGreen = 0;
-        for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
-            for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {
-                arrayResultOnlyGreen[indexGreen] = getRGB(0, 0, array2DGreen[xPixel][yPixel], 0);
+        for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {
+            for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
+                arrayResultOnlyGreen[indexGreen] = getRGB(array2DAlpha[xPixel][yPixel], 0, array2DGreen[xPixel][yPixel], 0);
                 indexGreen += 1;
             }
         }
@@ -55,9 +56,9 @@ public class Main {
         // blue channel
         int[] arrayResultOnlyBlue = new int[image.getWidth() * image.getHeight()];
         int indexBlue = 0;
-        for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
-            for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {
-                arrayResultOnlyBlue[indexBlue] = getRGB(0, 0, 0, array2DBlue[xPixel][yPixel]);
+        for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {
+            for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
+                arrayResultOnlyBlue[indexBlue] = getRGB(array2DAlpha[xPixel][yPixel], 0, 0, array2DBlue[xPixel][yPixel]);
                 indexBlue += 1;
             }
         }
@@ -65,34 +66,37 @@ public class Main {
         bmp = new BMP(image.getWidth(), image.getHeight(), (short) 32, arrayResultOnlyBlue);
         bmp.saveBMP("only-blue.bmp");
 
-        // with custom alfa-channel
-        int[] arrayResultWithCustomAlfa = new int[image.getWidth() * image.getHeight()];
+        // original image duplicate (for testing)
+        int[] arrayResultOriginal = new int[image.getWidth() * image.getHeight()];
         int indexCustomAlfa = 0;
-        for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
-            for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {
-                arrayResultWithCustomAlfa[indexCustomAlfa] = getRGB(
+        for (int yPixel = 0; yPixel < image.getHeight(); yPixel++) {
+            for (int xPixel = 0; xPixel < image.getWidth(); xPixel++) {
+                arrayResultOriginal[indexCustomAlfa] = getRGB(
                         array2DAlpha[xPixel][yPixel], array2DRed[xPixel][yPixel],
                         array2DGreen[xPixel][yPixel], array2DBlue[xPixel][yPixel]);
                 indexCustomAlfa += 1;
             }
         }
 
-        bmp = new BMP(image.getWidth(), image.getHeight(), (short) 32, arrayResultWithCustomAlfa);
-        bmp.saveBMP("custom-alfa.bmp");
+        BufferedImage bufferedImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+        bufferedImage.setRGB(0, 0, image.getWidth(), image.getHeight(), arrayResultOriginal, 0, image.getWidth());
+        ImageIO.write(bufferedImage, "bmp", new File("original-image.bmp"));
     }
 
     private static int getRGB(int alpha, int red, int green, int blue) {
         return ((alpha & 0xFF) << 24) | ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | ((blue & 0xFF) << 0);
     }
 
-    public static String toBinary(int x, int len) {
-        final char[] buff = new char[len];
+    private static String intToBinaryString(int x) {
+        return toBinaryStringWithLength(x, 32);
+    }
 
+    private static String toBinaryStringWithLength(int x, int len) {
+        final char[] buff = new char[len];
         for (int i = len - 1; i >= 0; i--) {
             int mask = 1 << i;
             buff[len - 1 - i] = (x & mask) != 0 ? '1' : '0';
         }
-
         return new String(buff);
     }
 }
