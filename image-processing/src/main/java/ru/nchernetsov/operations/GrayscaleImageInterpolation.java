16e3ab79039ca8cd32b8e4d8b6645b2edcc016d7
Nikita Chernetsov <nchernetsov@yandex-team.ru>
2021-11-10 14:59:10 +0300

    Добавлены заготовки методов для масштабирования монохромного изображения

--- image-processing/src/main/java/ru/nchernetsov/operations/GrayscaleImageInterpolation.java
+++ image-processing/src/main/java/ru/nchernetsov/operations/GrayscaleImageInterpolation.java
@@ -1,4 +1,41 @@
 package ru.nchernetsov.operations;
 
+import ru.nchernetsov.GrayscaleImage;
+
 public class GrayscaleImageInterpolation {
+
+    /**
+     * Изменение размеров монохромного изображения в заданное количество раз с интерполяцией яркости по ближайшему
+     * соседу
+     *
+     * @param original    исходное изображение
+     * @param coefficient коэффициент изменения размеров изображения
+     * @return масштабированное изображение
+     */
+    public static GrayscaleImage nearestNeighborInterpolation(GrayscaleImage original, double coefficient) {
+        return null;
+    }
+
+    /**
+     * Изменение размеров монохромного изображения в заданное количество раз с билинейной интерполяцией яркости
+     *
+     * @param original    исходное изображение
+     * @param coefficient коэффициент изменения размеров изображения
+     * @return масштабированное изображение
+     */
+    public static GrayscaleImage bilinearInterpolation(GrayscaleImage original, double coefficient) {
+        return null;
+    }
+
+    /**
+     * Изменение размеров монохромного изображения в заданное количество раз с бикубической интерполяцией яркости
+     *
+     * @param original    исходное изображение
+     * @param coefficient коэффициент изменения размеров изображения
+     * @return масштабированное изображение
+     */
+    public static GrayscaleImage bicubicInterpolation(GrayscaleImage original, double coefficient) {
+        return null;
+    }
+
 }
